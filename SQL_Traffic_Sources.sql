7859c6e7d4fc63ea17e9d98f574ac9ff6148b371
annadutkiewicz <33380648+annadutkiewicz@users.noreply.github.com>
2020-03-11 10:59:09 +0100

    Add files via upload

--- SQL_Traffic_Sources.sql
+++ SQL_Traffic_Sources.sql
@@ -58,3 +58,37 @@ GROUP BY
 /*Result: Non brand traffic seem to be sensitive to bid changes and volume is down,
 so we are goint to monitor volume traffic and think about how to make the campaigns
 more efficient to increase volume again*/
+    
+SELECT
+	website_sessions.device_type,
+    COUNT(DISTINCT website_sessions.website_session_id) AS sessions,
+    COUNT(DISTINCT orders.order_id) AS orders,
+    COUNT(DISTINCT orders.order_id) / COUNT(DISTINCT website_sessions.website_session_id) AS session_to_order_conv_rate
+FROM website_sessions
+	LEFT JOIN orders
+		ON website_sessions.website_session_id = orders.website_session_id
+WHERE
+	website_sessions.created_at < '2012-05-11' AND
+    utm_source = 'gsearch' AND
+    utm_campaign = 'nonbrand'
+GROUP BY
+	website_sessions.device_type
+
+/*Result: For desktop versions, conversion rate equals to 3.7%, for mobile traffic
+it is less than 1%. We are going to increase bids for desktop and analyze if bid changes
+make an impact*/
+
+SELECT
+    MIN(DATE(created_at)) AS week_start_date,
+	COUNT(DISTINCT CASE WHEN device_type = 'desktop' THEN website_session_id ELSE NULL END) AS dtop_sessions,
+    COUNT(DISTINCT CASE WHEN device_type = 'mobile' THEN website_session_id ELSE NULL END) AS mob_sessions
+FROM website_sessions
+WHERE
+	(created_at BETWEEN '2012-04-15' AND '2012-06-09') AND
+    utm_source = 'gsearch' AND
+    utm_campaign = 'nonbrand'
+GROUP BY
+	YEAR(created_at),
+    WEEK(created_at)
+    
+/*Result: 
