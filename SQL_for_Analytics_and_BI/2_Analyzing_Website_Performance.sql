f6c29e6300a3e5448736a35579f7aaa95fbb444a
Anna <anna.natalia.dutkiewicz@gmail.com>
2020-03-15 21:58:35 +0100

    update

--- SQL_for_Analytics_and_BI/2_Analyzing_Website_Performance.sql
+++ SQL_for_Analytics_and_BI/2_Analyzing_Website_Performance.sql
@@ -183,3 +183,88 @@ SELECT
 FROM sessions_w_counts_lander_and_created_at
 GROUP BY
 	YEARWEEK(session_created_at);
+    
+###################################################################################################################
+
+#6. BUILDING CONVERSION FUNNELS
+#STEP 1: select all pageviews for relevant sessions and identify each pageview as specific funnel step
+CREATE TEMPORARY TABLE session_level_made_it_flags
+SELECT
+	website_session_id,
+    MAX(products_page) AS products_made_it,
+    MAX(mrfuzzy_page) AS mrfuzzy_made_it,
+    MAX(cart_page) AS cart_made_it,
+    MAX(shipping_page) AS shipping_made_it,
+    MAX(billing_page) AS billing_made_it,
+    MAX(thankyou_page) AS thankyou_made_it
+FROM (
+SELECT
+	website_sessions.website_session_id,
+    website_pageviews.pageview_url,
+    #website_pageviews.created_at AS pageview_created_at,
+    CASE WHEN pageview_url = '/products' THEN 1 ELSE 0 END AS products_page,
+    CASE WHEN pageview_url = '/the-original-mr-fuzzy' THEN 1 ELSE 0 END AS mrfuzzy_page,
+    CASE WHEN pageview_url = '/cart' THEN 1 ELSE 0 END AS cart_page,
+    CASE WHEN pageview_url = '/shipping' THEN 1 ELSE 0 END AS shipping_page,
+    CASE WHEN pageview_url = '/billing' THEN 1 ELSE 0 END AS billing_page,
+    CASE WHEN pageview_url = '/thank-you-for-your-order' THEN 1 ELSE 0 END AS thankyou_page
+FROM website_sessions
+	LEFT JOIN website_pageviews
+		ON website_sessions.website_session_id = website_pageviews.website_session_id
+WHERE website_sessions.created_at BETWEEN '2012-08-05' AND '2012-09-05'
+	AND website_sessions.utm_campaign = 'nonbrand'
+    AND website_sessions.utm_source = 'gsearch'
+ORDER BY
+	website_pageviews.website_session_id,
+    website_pageviews.created_at
+) AS pageview_level
+GROUP BY
+	website_session_id;
+
+#STEP 2: create session-level conversion funnel view
+SELECT
+	COUNT(DISTINCT website_session_id) AS sessions,
+    COUNT(DISTINCT CASE WHEN products_made_it = 1 THEN website_session_id ELSE NULL END) AS to_products,
+    COUNT(DISTINCT CASE WHEN mrfuzzy_made_it = 1 THEN website_session_id ELSE NULL END) AS to_mrfuzzy,
+    COUNT(DISTINCT CASE WHEN cart_made_it = 1 THEN website_session_id ELSE NULL END) AS to_cart,
+    COUNT(DISTINCT CASE WHEN shipping_made_it = 1 THEN website_session_id ELSE NULL END) AS to_shipping,
+    COUNT(DISTINCT CASE WHEN billing_made_it = 1 THEN website_session_id ELSE NULL END) AS to_billing,
+    COUNT(DISTINCT CASE WHEN thankyou_made_it = 1 THEN website_session_id ELSE NULL END) AS to_thankyou
+FROM session_level_made_it_flags;
+
+#STEP 3: aggregate data to assess funnel performance
+SELECT
+    COUNT(DISTINCT CASE WHEN products_made_it = 1 THEN website_session_id ELSE NULL END) /
+		COUNT(DISTINCT website_session_id) AS lander_click_rt,
+    COUNT(DISTINCT CASE WHEN mrfuzzy_made_it = 1 THEN website_session_id ELSE NULL END) /
+		COUNT(DISTINCT CASE WHEN products_made_it = 1 THEN website_session_id ELSE NULL END) AS products_click_rt,
+    COUNT(DISTINCT CASE WHEN cart_made_it = 1 THEN website_session_id ELSE NULL END) /
+		COUNT(DISTINCT CASE WHEN mrfuzzy_made_it = 1 THEN website_session_id ELSE NULL END) AS mrfuzzy_click_rt,
+    COUNT(DISTINCT CASE WHEN shipping_made_it = 1 THEN website_session_id ELSE NULL END) /
+		COUNT(DISTINCT CASE WHEN cart_made_it = 1 THEN website_session_id ELSE NULL END) AS cart_click_rt,
+    COUNT(DISTINCT CASE WHEN billing_made_it = 1 THEN website_session_id ELSE NULL END) /
+		COUNT(DISTINCT CASE WHEN shipping_made_it = 1 THEN website_session_id ELSE NULL END) AS shipping_click_rt,
+    COUNT(DISTINCT CASE WHEN thankyou_made_it = 1 THEN website_session_id ELSE NULL END) /
+		COUNT(DISTINCT CASE WHEN billing_made_it = 1 THEN website_session_id ELSE NULL END) AS billing_click_rt
+FROM session_level_made_it_flags;
+
+#7. ANALYZING CONVERSION FUNNEL TESTS
+#STEP 1: finding the first time /billing-2 was seen
+SELECT
+	MIN(created_at) AS first_created_at,
+    website_pageview_id AS first_pv_id
+FROM website_pageviews
+WHERE pageview_url = '/billing-2';
+
+#STEP 2: final test analysis output
+SELECT
+	website_pageviews.pageview_url AS billing_version_seen,
+    COUNT(DISTINCT website_pageviews.website_session_id) AS sessions,
+    COUNT(DISTINCT orders.order_id) AS orders,
+    COUNT(DISTINCT orders.order_id) / COUNT(DISTINCT website_pageviews.website_session_id) AS billing_to_order_rt
+FROM website_pageviews
+	LEFT JOIN orders
+		ON website_pageviews.website_session_id = orders.website_session_id
+WHERE website_pageviews.created_at BETWEEN '2012-09-10 00:13:05' AND '2012-11-10'
+	AND website_pageviews.pageview_url IN ('/billing', '/billing-2')
+GROUP BY website_pageviews.pageview_url;
