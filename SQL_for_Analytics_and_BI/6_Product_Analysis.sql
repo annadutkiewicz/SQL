e8a02c09a9b863bfd7457483879e5ae6b3235e40
Anna <anna.natalia.dutkiewicz@gmail.com>
2020-03-20 21:55:17 +0100

    new exercises

--- SQL_for_Analytics_and_BI/6_Product_Analysis.sql
+++ SQL_for_Analytics_and_BI/6_Product_Analysis.sql
@@ -195,3 +195,131 @@ GROUP BY 1
 ###################################################################################################################
 
 #5. CROSS SELL ANALYSIS
+#STEP 1: identify the relevant /cart page views and their sessions
+CREATE TEMPORARY TABLE sessions_seeing_cart
+SELECT
+	CASE
+		WHEN created_at < '2013-09-25' THEN 'A. Pre_Cross_Sell'
+        WHEN created_at >= '2013-09-25' THEN 'B. Post_Cross_Sell'
+        ELSE 'uh oh...check logic'
+	END AS time_period,
+	website_session_id AS cart_session_id,
+	website_pageview_id AS cart_pageview_id
+FROM website_pageviews
+WHERE created_at BETWEEN '2013-08-25' AND '2013-10-25'
+	AND pageview_url = '/cart'
+;
+
+#STEP 2: see which of those /cart sessions clicked through the shipping page
+CREATE TEMPORARY TABLE cart_sessions_seeing_another_page
+SELECT
+	sessions_seeing_cart.time_period,
+    sessions_seeing_cart.cart_session_id,
+    MIN(website_pageviews.website_pageview_id) AS pv_id_after_cart
+FROM sessions_seeing_cart
+	LEFT JOIN website_pageviews
+		ON sessions_seeing_cart.cart_session_id = website_pageviews.website_session_id
+			AND sessions_seeing_cart.cart_pageview_id < website_pageviews.website_pageview_id
+GROUP BY
+	sessions_seeing_cart.time_period,
+    sessions_seeing_cart.cart_session_id
+HAVING 
+	MIN(website_pageviews.website_pageview_id) IS NOT NULL
+;
+
+#STEP 3: find the orders associated with /cart sessions; analyze products purchased, AOV; aggregate
+CREATE TEMPORARY TABLE pre_post_sessions_orders
+SELECT
+	time_period,
+    cart_session_id,
+    order_id,
+    items_purchased,
+    price_usd
+FROM sessions_seeing_cart
+	INNER JOIN orders
+		ON sessions_seeing_cart.cart_session_id = orders.website_session_id
+;
+
+SELECT
+	time_period,
+    COUNT(DISTINCT cart_session_id) AS cart_sessions,
+    SUM(clicked_to_another_page) AS clickthroughs,
+    SUM(clicked_to_another_page) / COUNT(DISTINCT cart_session_id) AS cart_ctr,
+    SUM(items_purchased) / SUM(placed_order) AS products_per_order,
+    SUM(price_usd) / SUM(placed_order) AS aov, #average order value
+    SUM(price_usd) / COUNT(DISTINCT cart_session_id) AS rev_per_cart_session
+FROM (
+SELECT
+	sessions_seeing_cart.time_period,
+    sessions_seeing_cart.cart_session_id,
+    CASE WHEN cart_sessions_seeing_another_page.cart_session_id IS NULL THEN 0 ELSE 1 END AS clicked_to_another_page,
+    CASE WHEN pre_post_sessions_orders.order_id IS NULL THEN 0 ELSE 1 END AS placed_order,
+    pre_post_sessions_orders.items_purchased,
+    pre_post_sessions_orders.price_usd
+FROM sessions_seeing_cart
+	LEFT JOIN cart_sessions_seeing_another_page
+		ON sessions_seeing_cart.cart_session_id = cart_sessions_seeing_another_page.cart_session_id
+	LEFT JOIN pre_post_sessions_orders
+		ON sessions_seeing_cart.cart_session_id = pre_post_sessions_orders.cart_session_id
+ORDER BY
+	cart_session_id
+) AS data_list
+GROUP BY
+	time_period
+;
+
+###################################################################################################################
+
+#6. PORTFOLIO EXPANSION ANALYSIS
+SELECT
+	CASE
+		WHEN website_sessions.created_at < '2013-12-12' THEN 'A. Pre_Birthday_Bear'
+        WHEN website_sessions.created_at >= '2013-12-12' THEN 'B. Post_Birthday_Bear'
+        ELSE 'uh oh...check logic'
+	END AS time_period,
+    #COUNT(DISTINCT website_sessions.website_session_id) AS sessions,
+    #COUNT(DISTINCT orders.order_id) AS orders,
+    COUNT(DISTINCT orders.order_id) / COUNT(DISTINCT website_sessions.website_session_id) AS conv_rate,
+    #SUM(orders.price_usd) AS total_revenue,
+    #SUM(orders.items_purchased) AS total_products_sold,
+    SUM(orders.price_usd) / COUNT(DISTINCT orders.order_id) AS aov,
+    SUM(orders.items_purchased) / COUNT(DISTINCT orders.order_id) AS products_per_order,
+    SUM(orders.price_usd) / COUNT(DISTINCT website_sessions.website_session_id) AS revenue_per_session
+FROM website_sessions
+	LEFT JOIN orders
+		ON website_sessions.website_session_id = orders.website_session_id
+WHERE website_sessions.created_at BETWEEN '2013-11-12' AND '2014-01-12'
+GROUP BY 1
+;
+
+###################################################################################################################
+
+#7. PRODUCT REFUND RATES
+SELECT
+	YEAR(order_items.created_at) AS yr,
+    MONTH(order_items.created_at) AS mo,
+    COUNT(DISTINCT CASE WHEN order_items.product_id = 1 THEN order_items.order_item_id ELSE NULL END) AS p1_orders,
+    #COUNT(DISTINCT CASE WHEN (order_items.product_id = 1 AND order_item_refunds.order_item_id IS NOT NULL) THEN order_item_refunds.order_id ELSE NULL END) AS p1_refund,
+    COUNT(DISTINCT CASE WHEN (order_items.product_id = 1 AND order_item_refunds.order_item_id IS NOT NULL) THEN order_item_refunds.order_id ELSE NULL END) /
+		COUNT(DISTINCT CASE WHEN order_items.product_id = 1 THEN order_items.order_item_id ELSE NULL END) AS p1_refund_rt,
+	
+    COUNT(DISTINCT CASE WHEN order_items.product_id = 2 THEN order_items.order_item_id ELSE NULL END) AS p2_orders,
+    #COUNT(DISTINCT CASE WHEN (order_items.product_id = 2 AND order_item_refunds.order_item_id IS NOT NULL) THEN order_item_refunds.order_id ELSE NULL END) AS p2_refund,
+    COUNT(DISTINCT CASE WHEN (order_items.product_id = 2 AND order_item_refunds.order_item_id IS NOT NULL) THEN order_item_refunds.order_id ELSE NULL END) /
+		COUNT(DISTINCT CASE WHEN order_items.product_id = 2 THEN order_items.order_item_id ELSE NULL END) AS p2_refund_rt,
+	
+    COUNT(DISTINCT CASE WHEN order_items.product_id = 3 THEN order_items.order_item_id ELSE NULL END) AS p3_orders,
+    #COUNT(DISTINCT CASE WHEN (order_items.product_id = 3 AND order_item_refunds.order_item_id IS NOT NULL) THEN order_item_refunds.order_id ELSE NULL END) AS p3_refund,
+    COUNT(DISTINCT CASE WHEN (order_items.product_id = 3 AND order_item_refunds.order_item_id IS NOT NULL) THEN order_item_refunds.order_id ELSE NULL END) /
+		COUNT(DISTINCT CASE WHEN order_items.product_id = 3 THEN order_items.order_item_id ELSE NULL END) AS p3_refund_rt,
+	
+    COUNT(DISTINCT CASE WHEN order_items.product_id = 4 THEN order_items.order_item_id ELSE NULL END) AS p4_orders,
+    #COUNT(DISTINCT CASE WHEN (order_items.product_id = 4 AND order_item_refunds.order_item_id IS NOT NULL) THEN order_item_refunds.order_id ELSE NULL END) AS p4_refund,
+    COUNT(DISTINCT CASE WHEN (order_items.product_id = 4 AND order_item_refunds.order_item_id IS NOT NULL) THEN order_item_refunds.order_id ELSE NULL END) /
+		COUNT(DISTINCT CASE WHEN order_items.product_id = 4 THEN order_items.order_item_id ELSE NULL END) AS p4_refund_rt
+FROM order_items
+	LEFT JOIN order_item_refunds
+		ON order_items.order_item_id = order_item_refunds.order_item_id
+WHERE order_items.created_at < '2014-10-15'
+GROUP BY 1,2
+;
