c35e02629b41a5736de72d38709390f4fc40cdaa
Anna <anna.natalia.dutkiewicz@gmail.com>
2020-03-21 21:13:55 +0100

    new exercises

--- SQL_for_Analytics_and_BI/6_Product_Analysis.sql
+++ SQL_for_Analytics_and_BI/6_Product_Analysis.sql
@@ -323,3 +323,68 @@ FROM order_items
 WHERE order_items.created_at < '2014-10-15'
 GROUP BY 1,2
 ;
+
+###################################################################################################################
+
+#8. IDENTIFYING REPEAT VISITORS
+SELECT
+	repeat_sessions,
+    COUNT(user_id) AS users
+FROM (
+	SELECT
+		user_id,
+        is_repeat_session,
+		SUM(is_repeat_session) AS repeat_sessions
+	FROM website_sessions
+	WHERE created_at BETWEEN '2014-01-01' AND '2014-11-01'
+	GROUP BY 1
+) AS sessions_number
+WHERE is_repeat_session = 0
+GROUP BY 1
+ORDER BY 1
+;
+
+###################################################################################################################
+
+#9. ANALYZING REPEAT BEHAVIOR
+CREATE TEMPORARY TABLE min_first_session
+SELECT
+	website_session_id,
+    is_repeat_session,
+    user_id,
+    MIN(created_at) as min_first
+FROM website_sessions
+WHERE created_at BETWEEN '2014-01-01' AND '2014-11-03'
+	AND is_repeat_session = 0
+GROUP BY user_id
+;
+
+CREATE TEMPORARY TABLE min_second_session
+SELECT
+	website_session_id,
+    is_repeat_session,
+    user_id,
+    MIN(created_at) AS min_second
+FROM website_sessions
+WHERE created_at BETWEEN '2014-01-01' AND '2014-11-03'
+	AND is_repeat_session = 1
+GROUP BY user_id
+;
+
+SELECT
+	AVG(days_first_to_second) AS avg_days_first_to_second,
+    MIN(days_first_to_second) AS min_days_first_to_second,
+    MAX(days_first_to_second) AS max_days_first_to_second
+FROM (
+SELECT
+	#user_id,
+	DATEDIFF(min_second, min_first) AS days_first_to_second
+FROM min_first_session
+	INNER JOIN min_second_session
+		ON min_first_session.user_id = min_second_session.user_id
+) AS subquery
+;
+
+###################################################################################################################
+
+#10.
