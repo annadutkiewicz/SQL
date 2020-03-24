a8df3986c455eab5b9c40cf666d7bd38f7d75458
Anna <anna.natalia.dutkiewicz@gmail.com>
2020-03-24 10:02:40 +0100

    add new exercises

--- SQL_for_Analytics_and_BI/7_Final_Project.sql
+++ SQL_for_Analytics_and_BI/7_Final_Project.sql
@@ -117,3 +117,66 @@ GROUP BY 1,2
 page, and show how the % of those sessions clicking through another page has changed over time, along with
 a view of how conversion from /products to placing an order has improved.
 */
+CREATE TEMPORARY TABLE products_pageviews
+SELECT
+	website_session_id,
+    website_pageview_id,
+    created_at AS saw_product_page_at
+FROM website_pageviews
+WHERE pageview_url = '/products'
+;
+
+SELECT
+	YEAR(products_pageviews.saw_product_page_at) AS yr,
+	MONTH(products_pageviews.saw_product_page_at) AS mo,
+    COUNT(DISTINCT products_pageviews.website_session_id) AS sessions_to_product_page,
+    COUNT(DISTINCT website_pageviews.website_session_id) AS clicked_to_next_page,
+    COUNT(DISTINCT website_pageviews.website_session_id) / COUNT(DISTINCT products_pageviews.website_session_id) AS clickthrough_rt,
+    COUNT(DISTINCT orders.order_id) AS orders,
+    COUNT(DISTINCT orders.order_id) / COUNT(DISTINCT products_pageviews.website_session_id) AS products_to_order_rt
+FROM products_pageviews
+	LEFT JOIN website_pageviews
+		ON products_pageviews.website_session_id = website_pageviews.website_session_id
+        AND products_pageviews.website_pageview_id < website_pageviews.website_pageview_id
+			LEFT JOIN orders
+				ON website_pageviews.website_session_id = orders.website_session_id
+GROUP BY 1,2
+;
+
+###################################################################################################################
+
+/*
+7. We made our 4th product available as a primary product on December 05, 2014 (it was previously only a cross sell
+item). Could you please pull sales data since then, and show how well each product cross sells from one another?
+*/
+CREATE TEMPORARY TABLE primary_products
+SELECT
+	order_id,
+    primary_product_id,
+    created_at AS ordered_at
+FROM orders
+WHERE created_at > '2014-12-05'
+;
+
+SELECT
+	primary_product_id,
+    COUNT(DISTINCT order_id) AS total_orders,
+	COUNT(CASE WHEN cross_sell_product_id = 1 THEN order_id ELSE NULL END) AS x_sell_1,
+    COUNT(CASE WHEN cross_sell_product_id = 2 THEN order_id ELSE NULL END) AS x_sell_2,
+    COUNT(CASE WHEN cross_sell_product_id = 3 THEN order_id ELSE NULL END) AS x_sell_3,
+    COUNT(CASE WHEN cross_sell_product_id = 4 THEN order_id ELSE NULL END) AS x_sell_4,
+	COUNT(CASE WHEN cross_sell_product_id = 1 THEN order_id ELSE NULL END) / COUNT(DISTINCT order_id) AS x_sell_1_rt,
+    COUNT(CASE WHEN cross_sell_product_id = 2 THEN order_id ELSE NULL END) / COUNT(DISTINCT order_id) AS x_sell_2_rt,
+    COUNT(CASE WHEN cross_sell_product_id = 3 THEN order_id ELSE NULL END) / COUNT(DISTINCT order_id) AS x_sell_3_rt,
+    COUNT(CASE WHEN cross_sell_product_id = 4 THEN order_id ELSE NULL END) / COUNT(DISTINCT order_id) AS x_sell_4_rt
+FROM (
+SELECT
+	primary_products.*,
+    order_items.product_id AS cross_sell_product_id
+FROM primary_products
+	LEFT JOIN order_items
+		ON primary_products.order_id = order_items.order_id
+        AND order_items.is_primary_item = 0
+) AS primary_w_cross_sell
+GROUP BY 1
+;
